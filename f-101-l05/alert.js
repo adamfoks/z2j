c29a7488ed40f953fd341dd1cc563b0368e75cac
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-01-05 20:18:38 +0100

    Variables tasks in new folder

---
+++ f-101-l05/alert.js
@@ -0,0 +1,14 @@
+let admin, name;
+
+name = "Adam";
+admin = name;
+alert(admin);
+
+
+let ourPlanetName = "Earth";
+let currentUserName = "Adam";
+
+
+const BIRTHDAY = '04.09.1981';
+
+alert(BIRTHDAY);

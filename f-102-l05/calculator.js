2540b4fd9cdfac1a9cd9a53306972fc2861d06bd
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-02-18 14:23:39 +0100

    adding best working solution: js commented

--- f-102-l05/calculator.js
+++ f-102-l05/calculator.js
@@ -77,3 +77,63 @@ while (
       operator = prompt("Podaj operator arytmetyczny (+, -, *, /, %):", "");
   }
 }
+
+
+
+
+// best working solution
+
+/* const inputNum1 = Number(prompt('Podaj pierwszą liczbę'));
+const arithmeticOperator = prompt('Podaj operator arytmetyczny (+, -, *, / lub %)');
+const inputNum2 = Number(prompt('Podaj drugą liczbę'));
+let error = '';
+let result = 0;
+ 
+function add(num1, num2) { return num1 + num2; }
+ 
+function subtract(num1, num2) { return num1 - num2; }
+ 
+function multiply(num1, num2) { return num1 * num2; }
+ 
+function divide(num1, num2) {
+  if (num2 === 0) return 'Nie dziel przez 0!';
+  else return num1 / num2;
+}
+ 
+function modulo(num1, num2) { return num1 % num2; }
+ 
+function calculate(num1, arithmeticOperator, num2) {
+  error = '';
+  if (isNaN(num1) || isNaN(num2)) {
+    error = 'Podane liczby są nieprawidłowe!';
+    return 0;
+  }
+ 
+  switch (arithmeticOperator) {
+    case '+': return add(num1, num2);
+    case '-': return subtract(num1, num2);
+    case '*': return multiply(num1, num2);
+    case '/': return divide(num1, num2);
+    case '%': return modulo(num1, num2);
+    default:
+      error = 'Nieprawidłowy operator!';
+      return 0;
+  }
+}
+ 
+function displayResult() {
+  if (error) alert(error);
+  else alert(result);
+}
+ 
+result = calculate(inputNum1, arithmeticOperator, inputNum2);
+displayResult();
+ 
+while(true) {
+  const nextArithmeticOperator = prompt('Podaj operator arytmetyczny (+, -, *, / lub %). Zostaw puste żeby zakończyć.');
+  if (!nextArithmeticOperator) break;
+ 
+  const nextNum = Number(prompt('Podaj kolejną liczbę'));
+  result = calculate(result, nextArithmeticOperator, nextNum);
+  displayResult();
+} */
